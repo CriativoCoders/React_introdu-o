562dc58623fbbee8bebdf9cb25fde53a074a6987
Talita Cristina Alves Lobato <05058225204@>
2025-04-08 08:11:34 -0300

    novo_tera atualizações

--- src/componentes/Lista.jsx
+++ src/componentes/Lista.jsx
@@ -1,46 +1,40 @@
-
-// axios permite a comunição com as paginas http (https)
+//axios permite a comunicação com as paginas http (https)
 import axios from "axios";
-// as ferrramentas começadas com use são hooks de manuseio da biblioteca React
+//as ferramentas começadas com use são hooks de manuseio da biblioteca React
 import React, {useState, useEffect} from "react";
-import { Card } from "./Card";
-import estilos from '.Lista.module.css';
+import { Card } from './Card';
+import estilos from './Lista.module.css';
 
-const API_key = 'af26cce282aecf5c6cc39a264f29d0a7'
-//colocar chave da api 
-const API_URL = 'https://api.themoviedb.org/3/'
-//enedreço da api ir em aplication 
+const API_key = 'af26cce282aecf5c6cc39a264f29d0a7';
+const API_URL = 'https://api.themoviedb.org/3';
 
 export function Lista(){
-//crio uma variavel chamada movie, e "seto" o estado dela como vazia 
+    //crio uma variavel chamada movie, e "seto" o estado dela como vazio
     const [movies, setMovie] = useState([]);
-
-    // Effect trabalha com uma estrutura especifiva; paremtros () cripts {} e dependencias []
+    
+    //Efect trabalha com uma estruta especifica parametros (), script {}, e dependencias []
 
     useEffect(()=>{
         axios.get(`${API_URL}/movie/popular?api_key=${API_key}&language=pt-BR`)
-        // se deu certo a comunicação, então grava o resultado no "Response" que pode pegar elemento atráves da data
-        .then(response =>{
-            console.log(response.data.results);
-            setMovie(response.data.results)
-
-        })
-        // se der ruim, exibe no inspecionar o erro de comunicação
-        .catch(error =>{
-            console.log('erro', error);
-
-        });
-
+            //se deu certo a comunicação, então grava o resultado no "response" que pode pegar elemento através do data
+            .then(response =>{
+                console.log(response.data.results);
+                setMovie(response.data.results)
+            })
+            //se der ruim, exibe no inspecionar o erro de comunicação
+            .catch(error =>{
+                console.log('Error', error);
+            });
     },[])
 
     return(
-        <div className={estilos.container}>
-            <figure style={{display:'flex', flexWrap:'wrap'}}>
-                {movies.map(movie=>{
+        <div className={estilos.conteiner}>
+            <figure style ={{display:'flex', flexWrap:'wrap'}}>
+                {movies.map(movie=>(
                     <Card key={movie.id}
                     movie={movie}/>
-                })}
+                ))}
             </figure>
         </div>
-    )
+    );
 }
